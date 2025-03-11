1e7aab8a80c8bfaa0e1d317c123c73b5aa44d7a2
jbill86 <158220237+jbill86@users.noreply.github.com>
2025-03-11 16:27:36 -0400

    Update vite.config.ts
    base: process.env.VITE_BASE_PATH || "/wyllie.crm", to line 83

--- vite.config.ts
+++ vite.config.ts
@@ -80,7 +80,7 @@ export default defineConfig({
     define: {
         'process.env': process.env,
     },
-    base: './',
+    base: process.env.VITE_BASE_PATH || "/wyllie.crm",
     esbuild: {
         keepNames: true,
     },
